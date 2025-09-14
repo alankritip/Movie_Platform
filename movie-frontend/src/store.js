f7f69647ad91d69209efc6f69fd348800c53f4fb
alankritip <alankritipal2004@gmail.com>
2025-09-14 18:04:40 +0530

    setting up reducers and configuration

---
+++ movie-frontend/src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { api } from './features/api/apiSlice';
+import authReducer from './features/auth/authSlice';
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
